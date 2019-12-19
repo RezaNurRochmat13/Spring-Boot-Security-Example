2e84ec3fab65544f5752f3b4d48ddd9033f3d5a5
rezanurrochmat@gmail.com <rezanurrochmat@gmail.com>
2019-12-19 13:46:02 +0700

    [Rejak] Add controller index

--- src/main/java/com/rejak/springsecurityexamples/index/controller/IndexController.java
+++ src/main/java/com/rejak/springsecurityexamples/index/controller/IndexController.java
@@ -1,13 +1,11 @@
 package com.rejak.springsecurityexamples.index.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/api/v1")
 public class IndexController {
-    @GetMapping("/")
+    @RequestMapping("/")
     public String indexPage() {
         return "Index service user";
     }
